918422ebbe35fc28a2df40a4eb8e49d5dbca9c08
vladimirbobkov-code <vladimirbobkov75@gmail.com>
2026-06-01 22:28:55 +0300

    Enable debug

--- fashion_store/settings.py
+++ fashion_store/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = 'django-insecure-0#s9j*2q#!2_)5zz$h&=16)bvx6y8o(x7hyu_f(0_@lg#4w-yc'
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = [
     '127.0.0.1',
