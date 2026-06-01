b7f17e368ae16d9a97d389c458b30dce0b0462c6
vladimirbobkov-code <vladimirbobkov75@gmail.com>
2026-06-01 22:14:23 +0300

    check media files

--- shop/views.py
+++ shop/views.py
@@ -1,10 +1,20 @@
 from django.shortcuts import render
 from .models import Product
 from django.core.mail import send_mail
+import os
 
 
 def home(request):
     products = Product.objects.all()
+
+    # Проверка файлов изображений
+    for p in products:
+        try:
+            print("URL:", p.image.url)
+            print("PATH:", p.image.path)
+            print("EXISTS:", os.path.exists(p.image.path))
+        except Exception as e:
+            print("ERROR:", e)
 
     if request.method == "POST":
         name = request.POST.get("name")
@@ -13,7 +23,7 @@ def home(request):
         send_mail(
             subject="Новая заявка с сайта",
             message=f"Имя: {name}\nТелефон: {phone}",
-            from_email=None,  # возьмется из settings
+            from_email=None,
             recipient_list=["vladimirbobkov75@gmail.com"],
             fail_silently=False,
         )
