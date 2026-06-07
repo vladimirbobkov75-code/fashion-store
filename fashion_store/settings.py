19bb8ae69a72355811ac8313d06544b94aa0befd
vladimirbobkov-code <vladimirbobkov75@gmail.com>
2026-06-07 21:04:43 +0300

    Switch database back to SQLite

--- fashion_store/settings.py
+++ fashion_store/settings.py
@@ -84,15 +84,8 @@ WSGI_APPLICATION = 'fashion_store.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'NAME': 'fashion_store',
-        'USER': 'root',
-        'PASSWORD': '12345678',
-        'HOST': 'localhost',
-        'PORT': '3306',
-        'OPTIONS': {
-            'charset': 'utf8mb4',
-        },
+        'ENGINE': 'django.db.backends.sqlite3',
+        'NAME': BASE_DIR / 'db.sqlite3',
     }
 }
 
